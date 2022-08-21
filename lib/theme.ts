d406dbbf5f41a2f73aee484004e0b1ac8ef24599
Pranav543 <pranavnaik543@gmail.com>
2022-08-21 23:54:07 +0530

    backend complete front end partially ready

---
+++ lib/theme.ts
@@ -0,0 +1,30 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    background: { 
+      default: "#F8F8F8"
+    },
+    primary: {
+      light: '#757ce8',
+      main: '#000000',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#000000',
+      main: '#ffffff',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    "fontFamily": `"Poppins", "Helvetica", "Arial", sans-serif`,
+    "fontSize": 14,
+    "fontWeightLight": 300,
+    "fontWeightRegular": 400,
+    "fontWeightMedium": 500
+   }
+});
+
+export default theme;
